6d41dd8a69ed2ae12b461757ac94e7a834a2fbb2
Victor Freitag <132359912+victorfreitag@users.noreply.github.com>
2024-03-14 13:53:45 -0300

    Add files via upload

---
+++ Relacoes/src/main/java/com/relacoes/entities/Departamento.java
@@ -0,0 +1,31 @@
+package com.relacoes.entities;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departamento")
+public class Departamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+	
+	private String nome;
+	
+	
+
+}
---
+++ Relacoes/src/main/java/com/relacoes/relacoesApplication.java
@@ -0,0 +1,13 @@
+package com.relacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class relacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(relacoesApplication.class, args);
+	}
+
+}
